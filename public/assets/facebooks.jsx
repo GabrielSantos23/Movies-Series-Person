5306c8f021ff78e9efa3806220d1a4ecdd6641e3
gabriel silva <gabriel.gs605@gmail.com>
2023-05-19 18:21:45 -0300

    first commit

--- public/assets/facebooks.jsx
+++ public/assets/facebooks.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
 
 const Facebook = () => {
   return (
-    <FacebookIcon
+    <svg
+      className='fill-white hover:fill-sky-500'
       xmlns='http://www.w3.org/2000/svg'
       width='20'
       height='20'
@@ -11,14 +11,8 @@ const Facebook = () => {
       fill='#fff'
     >
       <path d='M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385'></path>
-    </FacebookIcon>
+    </svg>
   );
 };
-const FacebookIcon = styled.svg`
-  fill: #fff;
 
-  &:hover {
-    fill: #2196f3;
-  }
-`;
 export default Facebook;
