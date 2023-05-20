c22c354bbfc338daf45cc6f122e1a2d38dca1313
gabriel silva <gabriel.gs605@gmail.com>
2023-05-19 21:18:50 -0300

    first commit

--- app/components/FavoriteItems/FavoriteMovies.tsx
+++ app/components/FavoriteItems/FavoriteMovies.tsx
@@ -70,12 +70,9 @@ const FavoriteMovies = () => {
           <ClipLoader color='#1d9bf0' loading={isLoading} size={40} />
         </div>
       ) : movies.length > 0 ? (
-        <div className='flex flex-wrap lg:justify-start justify-center gap-3'>
+        <div className='flex flex-wrap lg:justify-start justify-center gap-3  max-h-[370px] min-h-[370px]'>
           {movies.map((movie) => (
-            <div
-              className='flex flex-col max-h-[370px] min-h-[370px]'
-              key={movie.id}
-            >
+            <div className='flex flex-col' key={movie.id}>
               <Item item={movie} type={'tv'} />
             </div>
           ))}
